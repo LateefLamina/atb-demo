b76cb110f19509946e15939fb71c645ac89732bf
Lateef Adebanke Lamina <57659658+LateefLamina@users.noreply.github.com>
2022-07-08 14:51:44 +0100

    Update server.js

--- heroes-frontend/heroes-frontend/server.js
+++ heroes-frontend/heroes-frontend/server.js
@@ -14,6 +14,6 @@ app.get("*", function(req, res) {
 const port = process.env.PORT || 9091;
 
 /* eslint-disable no-console */
-app.listen(port, (4200) => {
+app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
